d22331620571e1a7b9cac920fd80d87f7e2ab3f7
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-10-28 16:11:49 -0300

    add gradient and stars to background

--- src/templates/global.jsx
+++ src/templates/global.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import "./global.sass"
 
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      {children}
+    </>
+  )
 }
 
 export default Layout
