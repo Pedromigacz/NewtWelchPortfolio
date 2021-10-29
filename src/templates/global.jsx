4b574d73969cdb12bd6247a3380c865002016fa3
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-10-29 15:55:47 -0300

    fix startsBackground html scrollbar hijack

--- src/templates/global.jsx
+++ src/templates/global.jsx
@@ -3,12 +3,12 @@ import "./global.sass"
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div id="starsContainer">
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
       <div id="topContainer">{children}</div>
-    </>
+    </div>
   )
 }
 
