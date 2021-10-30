461adfb21cbba51b1f1db65fc67897c54ece783a
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-10-30 20:18:25 -0300

    add graphqlQuery images

--- src/components/SecondContainer.jsx
+++ src/components/SecondContainer.jsx
@@ -1,14 +1,38 @@
 import React from "react"
 import * as styles from "../styles/SecondContainer.module.sass"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const SecondContainer = () => {
+  const {
+    allContentfulProject: { nodes },
+  } = useStaticQuery(graphql`
+    {
+      allContentfulProject {
+        nodes {
+          title
+          id
+          image {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              quality: 100
+              placeholder: TRACED_SVG
+            )
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <section className={styles.section}>
-      <div className={styles.cardDiv}>Image 1</div>
-      <div className={styles.cardDiv}>Image 2</div>
-      <div className={styles.cardDiv}>Image 3</div>
-      <div className={styles.cardDiv}>Image 4</div>
-      <div className={styles.cardDiv}>Image 5</div>
+      {nodes.map(project => (
+        <GatsbyImage
+          image={getImage(project.image)}
+          alt={project.title}
+          className={styles.cardDiv}
+        />
+      ))}
     </section>
   )
 }
