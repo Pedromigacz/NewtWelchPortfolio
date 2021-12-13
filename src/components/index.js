0b8b57f880a880eedeb291222b3ab78e26dc4043
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-12-13 15:35:53 -0300

    Add Slideshow component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as FirstContainer } from "./FirstContainer.jsx"
 export { default as SecondContainer } from "./SecondContainer.jsx"
+export { default as SlideShow } from "./SlideShow.jsx"
