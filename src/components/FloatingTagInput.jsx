d25318c9fa18fbdedd1153127765bb366038c33f
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-12-14 13:56:17 -0300

    add Floating tag input component

---
+++ src/components/FloatingTagInput.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import { motion } from "framer-motion"
+import * as styles from "../styles/FloatingTagInput.module.sass"
+
+const FloatingTagInput = ({ label, onChange, value, textArea }) => {
+  const [labelFocused, setLabelFocused] = useState(false)
+  return (
+    <span className={styles.inputContainer}>
+      <motion.label
+        initial={{ y: 36 }}
+        animate={{ y: value || labelFocused ? 10 : 36 }}
+      >
+        {label}
+      </motion.label>
+      {textArea ? (
+        <textarea
+          rows="8"
+          cols="54"
+          onFocus={e => setLabelFocused(true)}
+          onBlur={e => setLabelFocused(false)}
+          type="text"
+          onChange={onChange}
+          value={value}
+        />
+      ) : (
+        <input
+          onFocus={e => setLabelFocused(true)}
+          onBlur={e => setLabelFocused(false)}
+          type="text"
+          onChange={onChange}
+          value={value}
+        />
+      )}
+    </span>
+  )
+}
+
+export default FloatingTagInput
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FirstContainer } from "./FirstContainer.jsx"
 export { default as SecondContainer } from "./SecondContainer.jsx"
 export { default as SlideShow } from "./SlideShow.jsx"
+export { default as FloatingTagInput } from "./FloatingTagInput.jsx"
